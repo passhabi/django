826edad87de8fe314c0e613a9689fb432ccdcd31
passhabi <pedramas@live.com>
2024-05-01 17:21:41 +0330

    Add the SignUp form to the website.

--- Todo App/todolist/views.py
+++ Todo App/todolist/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+
 
 # Create your views here.
+
+
+def signup(request):
+    return render(request, 'todolist\signup.html', {'django_form': UserCreationForm()})
