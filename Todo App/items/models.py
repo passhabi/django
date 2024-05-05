3064d0d060568fdf814114bfd4103c415b5c9385
passhabi <pedramas@live.com>
2024-05-05 18:14:52 +0330

    Make the  model Todolist to save and manage todos.

--- Todo App/items/models.py
+++ Todo App/items/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todolist(models.Model):
+    title = models.CharField(null=False,blank=False, max_length=300)
+    description = models.TextField(blank=True)
+    due_date= models.DateTimeField(blank=True)
+    priority = models.SmallIntegerField(blank=True)
+
+    category = models.IntegerField(blank=True)
+
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    completion_time = models.DateTimeField(null=True, blank=True)  
+
