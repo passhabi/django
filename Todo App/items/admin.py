b092df8ad084fe57ab924eceea7af2f6abbd7a37
passhabi <pedramas@live.com>
2024-05-06 10:07:12 +0330

    Add the creation_date readonly field to admin page.

--- Todo App/items/admin.py
+++ Todo App/items/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from items.models import Todolist
 
+class ToodlistAdmin(admin.ModelAdmin):
+   readonly_fields = ['creation_date', ]
+
 # Register your models here.
-admin.site.register(Todolist)
+admin.site.register(Todolist, ToodlistAdmin)
