3ca25ab6c0ac67050d941e63888f12c559d2c070
passhabi <pedramas@live.com>
2024-06-10 15:00:42 +0330

    Adds django-debug-toolbar

--- ido app/ido/settings.py
+++ ido app/ido/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todos',
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+
+]
+
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
 ]
 
 ROOT_URLCONF = 'ido.urls'
